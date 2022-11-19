b3b33ef3b29c3d652c986e157d41a544ef728566
OlgaMatviichuk <109869771+OlgaMatviichuk@users.noreply.github.com>
2022-11-19 22:36:47 +0200

    Project migrated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import SearchEngine from "./SearchEngine";
+
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="text-center mt-5"> Weather Search Engine</h1>
+        <SearchEngine />
+      </div>
+      <div class="footnote text-center mb-5">
+        <span>
+          This project is coded by Olga Matviichuk and is
+          <a
+            href="https://github.com/OlgaMatviichuk/weather-react-app"
+            target="_blank"
+            class="open-source"
+            title="GitHub open source"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced
+          </a>
+        </span>
+      </div>{" "}
     </div>
   );
 }
-
-export default App;
