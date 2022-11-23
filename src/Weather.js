d5ce9c814550fbf221d2a3d8e9802778327eb274
OlgaMatviichuk <109869771+OlgaMatviichuk@users.noreply.github.com>
2022-11-23 21:36:31 +0200

    Weather update date added

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import CurrentDate from "./CurrentDate";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -8,11 +9,13 @@ export default function Weather(props) {
     console.log(response.data);
     setWeatherData({
       ready: true,
+      
       temperature: Math.round(response.data.main.temp),
       description: response.data.weather[0].description,
       wind: Math.round(response.data.wind.speed),
       humidity: response.data.main.humidity,
       city: response.data.name,
+      date: new Date(response.data.dt * 1000),
     });
   }
 
@@ -44,8 +47,10 @@ export default function Weather(props) {
           <div className="col-4 text-start">
         <h1>{weatherData.city}</h1>
         
-          <h6> Wednesday, October 5</h6>
-          <h6>21:05</h6>
+          <h6>
+            <CurrentDate date={weatherData.date} />
+              </h6>
+         
         
         </div>
           <div className="col-4 text-center" id="current-temperature">
