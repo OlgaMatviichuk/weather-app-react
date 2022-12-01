77bc9a00cd2df965183dcff4f218f06cb309c8b6
OlgaMatviichuk <109869771+OlgaMatviichuk@users.noreply.github.com>
2022-12-01 20:52:42 +0200

    Added new component for weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
+import WeatherForecast from "./WeatherForecast";
+
 
 
 export default function Weather(props) {
@@ -59,6 +61,7 @@ function search () {
             </div>
           </form>
           <WeatherInfo data={weatherData} />
+          <WeatherForecast />
         </div>
       </div>
     );
